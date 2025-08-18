9a10780f18d82b50d542e9a3a802975daeac1970
EC2 Default User <ec2-user@ip-172-31-14-43.ap-northeast-1.compute.internal>
2025-08-18 15:23:56 +0000

    docs:update graves_genre migration

--- db/migrate/20250802143824_create_grave_genres.rb
+++ db/migrate/20250802143824_create_grave_genres.rb
@@ -3,6 +3,7 @@ class CreateGraveGenres < ActiveRecord::Migration[6.1]
     create_table :grave_genres do |t|
       t.references :grave, null: false, foreign_key: true
       t.references :genre, null: false, foreign_key: true
+      t.bigint :genre_id
       t.timestamps
     end
   end
