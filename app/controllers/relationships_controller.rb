e80f44984f88aa653ddc2d994bad2e2483ae220b
EC2 Default User <ec2-user@ip-172-31-14-43.ap-northeast-1.compute.internal>
2025-10-07 07:21:15 +0000

    feat: Revise follow via Ajax

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,17 +1,30 @@
-class RelationshipsController < ApplicationController
-    before_action :authenticate_user! # Deviseなどでのログイン認証
-  
-    # フォローする
-    def create
-      @user = User.find(params[:relationship][:followed_id])
-      current_user.follow(@user)
+# app/controllers/relationships_controller.rb
 
-    end
-  
-    # フォローを外す
-    def destroy
-      user = Relationship.find(params[:id]).followed
-      current_user.unfollow(user)
-      redirect_back(fallback_location: root_path)
+class RelationshipsController < ApplicationController
+  before_action :authenticate_user!
+
+  # フォローする
+  def create
+    @user = User.find(params[:relationship][:followed_id])
+    current_user.follow(@user)
+
+    # JSからのリクエストに応答するよう設定
+    respond_to do |format|
+      format.html { redirect_to @user } # 通常のHTMLリクエストの場合
+      format.js                         # JSリクエストの場合 (create.js.erbを呼び出す)
     end
   end
+
+  # フォローを外す
+  def destroy
+    # ★ user を @user に変更
+    @user = Relationship.find(params[:id]).followed
+    current_user.unfollow(@user)
+
+    # ★ redirect_back を削除し、JSで応答するよう設定
+    respond_to do |format|
+      format.html { redirect_to @user } # 通常のHTMLリクエストの場合
+      format.js                         # JSリクエストの場合 (destroy.js.erbを呼び出す)
+    end
+  end
+end
